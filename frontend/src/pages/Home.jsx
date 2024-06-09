36a0e367c1c42b05ce74b684b587609fc06751f4
shabeerp15 <shabeerp15@gmail.com>
2024-06-09 10:25:46 +0530

    Add shadcn ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import { Button } from "../components/ui/button";
 import React from "react";
 
 const Home = () => {
-	return <div>Home Page</div>;
+	const handleButtonClick = () => {
+		console.log("Button clicked");
+	};
+
+	return (
+		<div className="">
+			<div>Home Page</div>
+			<Button variant="destructive" onClick={handleButtonClick}>
+				Button
+			</Button>
+		</div>
+	);
 };
 export default Home;
