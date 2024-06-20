7ac9c21f9f093e7ace5f4019e270017409bba37c
shabeerp15 <shabeerp15@gmail.com>
2024-06-20 15:02:51 +0530

    Addloading state

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -7,6 +7,10 @@ const Users = () => {
 
 	const { data, loading } = useAxios({ url: "/api/users" });
 
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
 	return (
 		<div>
 			<h1 className="text-3xl font-bold mb-4">Users List</h1>
